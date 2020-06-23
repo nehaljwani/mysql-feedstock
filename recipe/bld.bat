8097f3c9041aef993f5897464a102a59e4c815c6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-06-23 13:51:12 -0400

    8.0.19 (#8)
    * 8.0.19
    
    Change-Id: Ie8a6cc8246d05dd4764654f8f771a1a1fc947a05
    
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.7.3, and conda-forge-pinning 2020.06.16.21.09.39

---
+++ recipe/bld.bat
@@ -0,0 +1,40 @@
+if exist %LIBRARY_LIB%\event_openssl.lib (
+  set "libevent_openssl_lib=%LIBRARY_LIB%\event_openssl.lib"
+) else (
+  set "libevent_openssl_lib=%LIBRARY_LIB%\event.lib"
+)
+
+if exist %LIBRARY_LIB%\liblz4.lib (
+  set "lz4_lib=%LIBRARY_LIB%\liblz4.lib"
+) else (
+  set "lz4_lib=%LIBRARY_LIB%\lz4.lib"
+)
+
+cmake -S%SRC_DIR% -Bbuild -GNinja ^
+  -DCMAKE_CXX_STANDARD=14 ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCOMPILATION_COMMENT=conda-forge ^
+  -DWITH_UNIT_TESTS=OFF ^
+  -DWITH_ZLIB=system ^
+  -DWITH_ZSTD=system ^
+  -DWITH_LZ4=system ^
+  -DLZ4_SYSTEM_LIBRARY=%lz4_lib% ^
+  -DLIBEVENT_OPENSSL=%libevent_openssl_lib% ^
+  -DWITH_ICU=system ^
+  -DWITH_EDITLINE=system ^
+  -DWITH_PROTOBUF=system ^
+  -DWITH_LIBEVENT=system ^
+  -DWITH_BOOST=%SRC_DIR%\boost ^
+  -DDEFAULT_CHARSET=utf8 ^
+  -DDEFAULT_COLLATION=utf8_general_ci ^
+  -DINSTALL_INCLUDEDIR=include/mysql ^
+  -DINSTALL_MANDIR=share/man ^
+  -DINSTALL_DOCDIR=share/doc/mysql ^
+  -DINSTALL_DOCREADMEDIR=mysql ^
+  -DINSTALL_INFODIR=share/info ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DINSTALL_MYSQLSHAREDIR=share/mysql ^
+  -DINSTALL_SUPPORTFILESDIR=mysql/support-files
+
+cmake --build build --config Release
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,74 +1,48 @@
 #!/bin/bash
 
-# this script is based off the homebrew package:
-# https://github.com/Homebrew/homebrew-core/blob/master/Formula/mysql.rb
-
-# make sure we can find cpp on the linux CI service
-if [[ `uname` != "Darwin" ]]
-then
-    CPP_ROOT=`dirname ${CPP}`
-    export LC_ALL=C  # on osx sed chokes on non UTF-8
-    find . -type f -print0 | xargs -0 sed -i"" -e "s|COMMAND rpcgen  -C|COMMAND rpcgen  -Y ${CPP_ROOT} -C|g"
-    unset LC_ALL
-
-    # make sure the cmake build can find everything
-    ln -s ${CPP} `dirname ${CPP}`/cpp
-else
-    export CPPFLAGS=${CPPFLAGS%" -mmacosx-version-min=10.9"}
+_rpcgen_hack_dir=""
+if [[ $target_platform == linux-64 ]]; then
+    _rpcgen_hack_dir=$SRC_DIR/rpcgen_hack
+    mkdir -p $_rpcgen_hack_dir/bin
+    cat <<EOF > ${_rpcgen_hack_dir}/bin/rpcgen
+#!/bin/bash
+$(patchelf --print-interpreter $CPP) $($CXX --print-sysroot)/usr/bin/rpcgen -Y ${_rpcgen_hack_dir}/bin \$@
+EOF
+    ln -s $(readlink -f ${CPP}) ${_rpcgen_hack_dir}/bin/cpp
+    chmod +x ${_rpcgen_hack_dir}/bin/{rpcgen,cpp}
 fi
 
-mkdir -p build
-cd build
+declare -a _xtra_cmake_args
+if [[ $target_platform == osx-64 ]]; then
+    _xtra_cmake_args+=(-DWITH_ROUTER=OFF)
+fi
 
-export CXXFLAGS="-fpermissive "${CXXFLAGS}
+cmake -S$SRC_DIR -Bbuild -GNinja \
+  -DCMAKE_CXX_STANDARD=14 \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH="${_rpcgen_hack_dir};$PREFIX" \
+  -DCOMPILATION_COMMENT=conda-forge \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
+  -DWITH_UNIT_TESTS=OFF \
+  -DWITH_SASL=system \
+  -DWITH_ZLIB=system \
+  -DWITH_ZSTD=system \
+  -DWITH_LZ4=system \
+  -DWITH_ICU=system \
+  -DWITH_EDITLINE=system \
+  -DWITH_PROTOBUF=system \
+  -DWITH_LIBEVENT=system \
+  -DWITH_BOOST=$SRC_DIR/boost \
+  -DDEFAULT_CHARSET=utf8 \
+  -DDEFAULT_COLLATION=utf8_general_ci \
+  -DINSTALL_INCLUDEDIR=include/mysql \
+  -DINSTALL_MANDIR=share/man \
+  -DINSTALL_DOCDIR=share/doc/mysql \
+  -DINSTALL_DOCREADMEDIR=mysql \
+  -DINSTALL_INFODIR=share/info \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DINSTALL_MYSQLSHAREDIR=share/mysql \
+  -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
+  "${_xtra_cmake_args[@]}"
 
-# -DINSTALL_* are relatiove to -DCMAKE_INSTALL_PREFIX
-# I checked and the bundled libs don't cause any issues with stomping
-# on the conda-forge packages. Thus I am going to go with them for now since
-# with the anaconda compilers I cannot convince the build to link properly.
-mkdir -p ${PREFIX}/mysql
-cmake \
-    -DCMAKE_C_FLAGS=${CFLAGS}" "${LDFLAGS} \
-    -DCMAKE_CXX_FLAGS=${CXXFLAGS}" "${LDFLAGS} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DINSTALL_INCLUDEDIR=include/mysql \
-    -DINSTALL_MANDIR=share/man \
-    -DINSTALL_DOCDIR=share/doc/mysql \
-    -DINSTALL_DOCREADMEDIR=mysql \
-    -DINSTALL_INFODIR=share/info \
-    -DINSTALL_MYSQLSHAREDIR=share/mysql \
-    -DINSTALL_SUPPORTFILESDIR=mysql/support-files \
-    -DINSTALL_SCRIPTDIR=mysql/scripts \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_FIND_FRAMEWORK=LAST \
-    -DCMAKE_VERBOSE_MAKEFILE=OFF \
-    -Wno-dev \
-    -DWITH_UNIT_TESTS=OFF \
-    -DDEFAULT_CHARSET=utf8 \
-    -DDEFAULT_COLLATION=utf8_general_ci \
-    -DCOMPILATION_COMMENT=conda-forge \
-    -DWITH_SSL=bundled \
-    -DWITH_EDITLINE=bundled \
-    -DWITH_BOOST=bundled \
-    -DDOWNLOAD_BOOST=1 \
-    .. &> cmake.log
-
-make
-make install &> install.log
-
-# remove this dir so we do not ship it
-cd ${PREFIX}/mysql-test
-mysql_temp_dir=`mktemp -d ${TMPDIR}/tmp/XXXXXXXXXXXX`
-{
-    set -e
-    # the || here is a rough try...except
-    perl mysql-test-run.pl status --vardir=${mysql_temp_dir} || rm -rf ${mysql_temp_dir}
-}
-cd -
-# always delete anything left
-rm -rf ${mysql_temp_dir}
-rm -rf ${PREFIX}/mysql-test
-
-# Make a symlink to script to start the server directly.
-ln -s ${PREFIX}/mysql/support-files/mysql.server ${PREFIX}/bin/mysql.server
+cmake --build build
